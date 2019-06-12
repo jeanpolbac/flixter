4f9082a51ffbb377c08146a837dc276e132252a1
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-06-12 15:33:55 +0000

    added student view page for courses and added link to nav bar and header image

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#index'
-
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
-  
+
   resources :users,  only: :show
 end
