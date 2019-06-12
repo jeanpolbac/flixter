15577ce809b305c26729caab8adb0d0129ebaeb3
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-06-12 19:30:59 +0000

    attempting to add image uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_161057) do
+ActiveRecord::Schema.define(version: 2019_06_12_191459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_161057) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -32,6 +33,11 @@ ActiveRecord::Schema.define(version: 2019_06_12_161057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["section_id"], name: "index_lessons_on_section_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sections", force: :cascade do |t|
