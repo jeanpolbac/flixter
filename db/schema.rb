ffaf50e320cc48faeebb0252d4156dbeb6e65a6d
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-06-14 21:10:33 +0000

    applied fixes to image uploader config  provided by my TA to avoid further issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_161057) do
+ActiveRecord::Schema.define(version: 2019_06_14_210436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_161057) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -32,6 +33,17 @@ ActiveRecord::Schema.define(version: 2019_06_12_161057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["section_id"], name: "index_lessons_on_section_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.string "image"
+    t.integer "user_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_photos_on_course_id"
+    t.index ["user_id", "course_id"], name: "index_photos_on_user_id_and_course_id"
   end
 
   create_table "sections", force: :cascade do |t|
