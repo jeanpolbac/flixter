4a1ab1dc84b5eba3f44206ab88d107a81c395b58
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-06-17 19:34:38 +0000

    found bug causing adding a section/lesson to not appear, now trying to resolve video upload error

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -19,7 +19,7 @@ class Instructor::SectionsController < ApplicationController
     end
   end
   
-  helper_method :current_user
+  helper_method :current_course
   def current_course
     @current_course ||= Course.find(params[:course_id])
   end
